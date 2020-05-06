2175d8722bd022efdc36a7ec3b60272284928a26
João Gabriel de Medeiros Pereira <gah.mp1@gmail.com>
2020-05-06 18:58:46 -0300

    ultimas alteracoes no jewFeatures

---
+++ JewFeatures/Classes/SharedFeatures/BFFTextfields/BFFTextfields.swift
@@ -0,0 +1,8 @@
+//
+//  BFFTextfields.swift
+//  JewFeatures
+//
+//  Created by Joao Gabriel Pereira on 24/04/20.
+//
+
+import Foundation
---
+++ JewFeatures/Classes/SharedFeatures/RequestSemaphore/RequestSemaphore.swift
@@ -0,0 +1,8 @@
+//
+//  RequestSemaphore.swift
+//  JewFeatures
+//
+//  Created by Joao Gabriel Pereira on 25/04/20.
+//
+
+import Foundation
